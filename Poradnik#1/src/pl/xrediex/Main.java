d91bafd07c237691d00de7508fb67a6696b983a9
xRediex <77249991+xRediex@users.noreply.github.com>
2021-01-11 11:01:00 +0100

    Add files via upload

---
+++ Poradnik#1/src/pl/xrediex/Main.java
@@ -0,0 +1,17 @@
+package pl.xrediex;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getCommand("pomoc").setExecutor(new Pomoc());//teraz event przy on plg :)
+        Bukkit.getConsoleSender().sendMessage("Plg sie onn wtf?");
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.getConsoleSender().sendMessage("Ehh ide spac");//dzk za ogladanie mam nadzieje ze sie podobało :) 
+    }
+}
